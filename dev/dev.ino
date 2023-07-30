825750876486e40174a36d81343feb6d574f3352
Yuyu378 <70588826+Yuyu378@users.noreply.github.com>
2023-07-31 00:48:17 +0800

    Modified files to concise function codes

--- dev/dev.ino
+++ dev/dev.ino
@@ -7,15 +7,15 @@
 #include <SoftwareSerial.h>
 SoftwareSerial MyBlue(9, 10); // RX | TX
 
-#define DATA_num 32
+#define DATA_num    32
 #define SAMPLE_FREQ 4
-#define INPUT_PIN 18
-#define DEL 3
+#define INPUT_PIN   18
+#define DEL         3
 
 const int ref = 400;
 unsigned int flag = 0, bb = 0, bbo = 0;
 int owo = 0, owo1 = 0;
-int data[DATA_num] = {0};
+int data[DATA_num] = { 0 };
 int r = 0, r1 = 0;
 int v0 = 0, v1 = 0, v2 = 0, v3 = 0;
 int sum = 0;
@@ -30,17 +30,17 @@ int flag2 = 0;
 int adc_value = 0;
 int increase = 0, decrease = 0;
 int samp2 = 0, samp3 = 0;
-int adc_arry[500];
+int adc_arry[500] = { 0 };
 int adc_times = 0;
 int up = 0, down = 0;
 int gain = 2; //===================
 
 // RR input
-float RR_interval[DATA_num];
-float delta_RR[DATA_num - 2 * DEL];
-float new_RR[DATA_num - 2 * DEL];
+float RR_interval[DATA_num] = { 0 };
+float delta_RR[DATA_num - 2 * DEL] = { 0 };
+float new_RR[DATA_num - 2 * DEL] = { 0 };
 int index = 0;
-int HR_RR[5];
+int HR_RR[5] = { 0 };
 int HR_times = 0;
 int HR_sum = 0;
 
@@ -318,6 +318,7 @@ void fill_RR(float *RR, float *delta_RR, int sz) {
     for (int i = 0; i < sz - 1; i++) {
         delta_RR[i] = RR[i + 1] - RR[i]; // 計算RR差值 = RR[i+1]-RR[i]
     }
+    return;
 }
 
 float Mean(float *RR, int sz) {
@@ -331,14 +332,13 @@ float Mean(float *RR, int sz) {
 }
 
 float SDNN(float *RR, int sz) {
-    float sum = 0.0;
+    float sum = 0.F;
     float avg = Mean(RR, sz);
     int N = sz;
     while (sz > 0) {
         sum += (RR[sz - 1] - avg) * (RR[sz - 1] - avg);
         sz--;
     }
-
     return sqrt(sum / (N - 1));
 }
 
@@ -357,23 +357,18 @@ float RMSSD(float *RR, float *delta_RR, int sz) {
 
 /*
 float SDSD(float *RR, float *delta_RR, int sz){
-    int i;
-    float sum=0.0;
-    int N=sz;
-
-    for(i=0;i<sz-1;i++){ //*
-        sum+= (delta_RR[i]-Mean(delta_RR, N))*(delta_RR[i]-Mean(delta_RR, N));
+    float sum = 0.F;
+    for(int i = 0; i < sz - 1; i++) {
+        sum += (delta_RR[i] - Mean(delta_RR, sz)) * (delta_RR[i] - Mean(delta_RR, sz));
     }
-
-    return sqrt(sum/(N-1));
+    return sqrt(sum / (sz - 1));
 }
 */
 
 void sort(float *a, int sz) {
-    int i, j, tmp;
-    for (i = 1; i < sz; i++) {
-        j = i;
-        tmp = a[j];
+    for (int i = 1; i < sz; i++) {
+        int j = i;
+        int tmp = a[j];
         while (j > 0 && a[j - 1] > tmp) {
             a[j] = a[j - 1];
             j--;
@@ -384,87 +379,82 @@ void sort(float *a, int sz) {
 }
 
 void delete_data(float *RR, int sz) {
-    int i;
-    for (i = DEL; i < sz - DEL; i++) {
+    for (int i = DEL; i < sz - DEL; i++) {
         new_RR[i - DEL] = RR[i];
     }
     return;
 }
 
-float FFT(int in[], int N, float Frequency) {
+void FFT(int* in, int N, float Frequency) {
 
-    // 2^n, n: index
-    unsigned int data[12] = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048 };
-    int a, c1, f, o, x;
+    // n squared, where n is the index
+    unsigned int data[12] = { 
+        1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048 
+    };
 
-    a = N;                          // 取樣多少點
+    int o;
     for (int i = 0; i < 12; i++) {  // calculating the levels
-        if (data[i] <= a) {
+        if (data[i] <= N) {         // sampling points
+            // the degree of 2 (approximation)
             o = i;
         }
-    } // o : the degree of 2 (approximation)
+    }
 
-    int in_ps[data[o]] = {0};       // input for sequencing --> data[o] -> 一次抓取的資料點數量 (=N)
+    int in_ps[data[o]] = { 0 };     // input for sequencing --> data[o] -> 一次抓取的資料點數量 (=N)
 
     // FFT 轉換完出來會是N點的複數( Re + Im )
-    float out_r[data[o]] = {};      // real part of transform
-    float out_im[data[o]] = {};     // imaginory part of transform
+    float out_re[data[o]] = { 0 };  // real part of transform
+    float out_im[data[o]] = { 0 };  // imaginory part of transform
 
-    x = 0;
-    for (int b = 0; b < o; b++) {   // bit reversal (Rader's algorithm)
-        c1 = data[b];               // 1 2 4 8 .. (2^b)
-        f = data[o] / (c1 + c1);
+    for (int b = 0, x = 0; b < o; b++) {   // bit reversal (Rader's algorithm)
+        int c1 = data[b];           // 1 2 4 8 .. (2^b)
+        int f = data[o] / (c1 + c1);
         for (int j = 0; j < c1; j++) {
-            x = x + 1;
+            x += 1;
             in_ps[x] = in_ps[j] + f;
         }
     }
 
     // update input array as per bit reverse order
     for (int i = 0; i < data[o]; i++)  {
-        if (in_ps[i] < a) {
-            out_r[i] = in[in_ps[i]];
+        if (in_ps[i] < N) {
+            out_re[i] = in[in_ps[i]];
         }
-        if (in_ps[i] > a) {
-            out_r[i] = in[in_ps[i] - a];
+        if (in_ps[i] > N) {
+            out_re[i] = in[in_ps[i] - N];
         }
     }
 
-    int i10, i11, n1;
-    float e, c, s, tr, ti;
-
-    for (int i = 0; i < o; i++) {       // FFT
-        i10 = data[i];                  // overall values of sine / cosine
-        i11 = data[o] / data[i + 1];    // loop with similar sine / cosine
-        e = 360 / data[i + 1];
-        e = 0 - e;
-        n1 = 0;
+    for (int i = 0; i < o; i++) {           // FFT
+        int i10 = data[i];                  // overall values of sine / cosine
+        int i11 = data[o] / data[i + 1];    // loop with similar sine / cosine
+        int e = -360 / data[i + 1];
 
         for (int j = 0; j < i10; j++) {
-            c = cosine(e * j);
-            s = sine(e * j);
-            n1 = j;
+            float c = cosine(e * j);
+            float s = sine(e * j);
+            int n1 = j;
 
             for (int k = 0; k < i11; k++) {
-                tr = c * out_r[i10 + n1] - s * out_im[i10 + n1];
-                ti = s * out_r[i10 + n1] + c * out_im[i10 + n1];
+                float tr = c * out_re[i10 + n1] - s * out_im[i10 + n1];
+                float ti = s * out_re[i10 + n1] + c * out_im[i10 + n1];
 
-                out_r[n1 + i10] = out_r[n1] - tr;
-                out_r[n1] = out_r[n1] + tr;
+                out_re[n1 + i10] = out_re[n1] - tr;
+                out_re[n1] = out_re[n1] + tr;
 
                 out_im[n1 + i10] = out_im[n1] - ti;
                 out_im[n1] = out_im[n1] + ti;
 
-                n1 = n1 + i10 + i10;
+                n1 += i10 + i10;
             }
         }
     }
 
-    //---> here onward out_r contains amplitude and our_in conntains frequency (Hz)
+    //---> here onward out_re contains amplitude and our_in conntains frequency (Hz)
     // getting amplitude from complex number
     for (int i = 0; i < data[o - 1]; i++) {
         // to  increase the speed delete sqrt (magnitude)
-        out_r[i] = sqrt(out_r[i] * out_r[i] + out_im[i] * out_im[i]);
+        out_re[i] = sqrt(out_re[i] * out_re[i] + out_im[i] * out_im[i]);
         out_im[i] = i * Frequency / N;      // 分到的頻率 --> 取點 ( Fn=(n-1)*Fs/N )
 
         if (out_im[i] == 0) { }
@@ -472,52 +462,49 @@ float FFT(int in[], int N, float Frequency) {
             Serial.print(out_im[i]);
             Serial.print("Hz");             // ~ 取樣精度 (x-axis)
             // Serial.print("\t");          // un comment to print freuency bin
-            Serial.print((int)out_r[i] / 100);
+            Serial.print((int)out_re[i] / 100);
             Serial.print("  ");
-            fft_data[i] = out_r[i] / 100;
+            fft_data[i] = out_re[i] / 100;
         }
     }
     // Serial.println("=============");
 
     // peak detection
-    x = 0;
+    int x = 0;
     for (int i = 1; i < data[o - 1] - 1; i++) {
-        if (out_r[i] > out_r[i - 1] && out_r[i] > out_r[i + 1]) {
+        if (out_re[i] > out_re[i - 1] && out_re[i] > out_re[i + 1]) {
             // storage of peak number
             in_ps[x] = i;
-            x = x + 1;
+            x += 1;
         }
     }
 
-    s = 0;
-    c = 0;
     // rearraange as per magnitude
-    for (int i = 0; i < x; i++) {
+    for (int i = 0, c = 0; i < x; i++, c++) {
         for (int j = c; j < x; j++) {
-            if (out_r[in_ps[i]] < out_r[in_ps[j]]) {
-                s = in_ps[i];
+            if (out_re[in_ps[i]] < out_re[in_ps[j]]) {
+                float s = in_ps[i];
                 in_ps[i] = in_ps[j];
                 in_ps[j] = s;
             }
         }
-        c = c + 1;
     }
 
     // updating f_peak array (global variable)with descending order
     for (int i = 0; i < 5; i++) {
         f_peaks[i] = out_im[in_ps[i]];
     }
+    return;
 }
 
 float sine(int i) {
-
     int j = i;
-    float out;
+    float out = 0.F;
     while (j < 0) {
-        j = j + 360;
+        j += 360;
     }
     while (j > 360) {
-        j = j - 360;
+        j -= 360;
     }
     if (j > -1 && j < 91) {
         out = sine_data[j];
@@ -531,21 +518,18 @@ float sine(int i) {
     else if (j > 270 && j < 361) {
         out = -sine_data[360 - j];
     }
-
     return (out / 255);
 }
 
 float cosine(int i) {
-
     int j = i;
-    float out;
+    float out = 0.F;
     while (j < 0) {
-        j = j + 360;
+        j += 360;
     }
     while (j > 360) {
-        j = j - 360;
+        j -= 360;
     }
-
     if (j > -1 && j < 91) {
         out = sine_data[90 - j];
     }
@@ -558,6 +542,5 @@ float cosine(int i) {
     else if (j > 270 && j < 361) {
         out = sine_data[j - 270];
     }
-
     return (out / 255);
 }
